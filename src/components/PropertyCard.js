27ffbb00b5233bab83ecb9ef7a31c7e181d32be3
Ed Adkins <ejmadkins@gmail.com>
2022-06-23 11:50:25 +0100

    add email mailto

--- src/components/PropertyCard.js
+++ src/components/PropertyCard.js
@@ -1,5 +1,14 @@
 /* eslint-disable react/prop-types */
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBed,
+  faBath,
+  faSterlingSign,
+  faLocationPin,
+  faBuilding,
+  faEnvelope,
+} from "@fortawesome/free-solid-svg-icons";
 import "../styles/property-card.css";
 
 const PropertyCard = ({
@@ -14,12 +23,29 @@ const PropertyCard = ({
   return (
     <div className="property-card">
       <h1 className="property-card__title">{title}</h1>
-      <p className="property-card__type">{type}</p>
-      <p className="property-card__bathrooms">{bathrooms}</p>
-      <p className="property-card__bedrooms">{bedrooms}</p>
-      <p className="property-card__price">{price}</p>
-      <p className="property-card__city">{city}</p>
-      <p className="property-card__email">{email}</p>
+      <img
+        className="property-card__image"
+        src="https://loremflickr.com/400/240/"
+        alt="placeholder"
+      />
+      <p className="property-card__type">
+        <FontAwesomeIcon icon={faBuilding} /> {type}
+      </p>
+      <p className="property-card__bathrooms">
+        <FontAwesomeIcon icon={faBath} /> {bathrooms}
+      </p>
+      <p className="property-card__bedrooms">
+        <FontAwesomeIcon icon={faBed} /> {bedrooms}
+      </p>
+      <p className="property-card__price">
+        <FontAwesomeIcon icon={faSterlingSign} /> {price}
+      </p>
+      <p className="property-card__city">
+        <FontAwesomeIcon icon={faLocationPin} /> {city}
+      </p>
+      <a className="property-card__email" href={`mailto:${email}`}>
+        <FontAwesomeIcon icon={faEnvelope} /> Send Email
+      </a>
     </div>
   );
 };
