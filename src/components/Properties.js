de3dc4119378e265e36668bfc11bb669d117a449
Ed Adkins <ejmadkins@gmail.com>
2022-06-23 10:35:44 +0100

    fix issue with incorrect name for key

--- src/components/Properties.js
+++ src/components/Properties.js
@@ -1,5 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import PropertyCard from "./PropertyCard";
+import getProperty from "../requests/getProperty";
+import "../styles/properties.css";
 
-const Properties = () => <div>Properties Page</div>;
+const Properties = () => {
+  const [results, setResults] = useState([]);
 
+  useEffect(() => {
+    getProperty(setResults);
+  }, []);
+
+  return (
+    <div className="property-cards">
+      {results.map((property) => (
+        <PropertyCard
+          title={property.title}
+          type={property.type}
+          bathrooms={property.bathrooms}
+          bedrooms={property.bedrooms}
+          price={property.price}
+          city={property.city}
+          email={property.email}
+        />
+      ))}
+    </div>
+  );
+};
 export default Properties;
